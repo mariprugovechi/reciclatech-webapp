3b3922125bd83474b310e0f165a382828788bfd1
Marianna Prugovechi <marianna.barbosa@mandarin.com.br>
2023-09-12 09:23:18 -0300

    fix: next config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,2 @@
-const {
-  createVanillaExtractPlugin
-} = require('@vanilla-extract/next-plugin');
-const withVanillaExtract = createVanillaExtractPlugin();
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
-
-module.exports = withVanillaExtract(nextConfig);
